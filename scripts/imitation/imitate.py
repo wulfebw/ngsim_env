663b4b37d98de4ca869b4d663531e4840860ef87
blake wulfe <wulfebw@stanford.edu>
2017-12-20 18:34:54 -0800

    normalizing rewards so that the critic and recognition rewards have the same relative scale

--- scripts/imitation/imitate.py
+++ scripts/imitation/imitate.py
@@ -161,7 +161,8 @@ with tf.Session() as session:
         critic_final_scale=1.,
         recognition_initial_scale=0.,
         recognition_final_scale=0.2,
-        summary_writer=summary_writer
+        summary_writer=summary_writer,
+        normalize_rewards=True
     )
 
     session.run(tf.global_variables_initializer())
